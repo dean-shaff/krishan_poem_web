3040846670d3626f179f97873e98dcfb812020ae
Dean Shaff <dshaff001@gmail.com>
2015-01-08 09:39:35 +0400

    Installed dependencies

--- poem_maker/settings.py
+++ poem_maker/settings.py
@@ -26,7 +26,6 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -93,7 +92,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
