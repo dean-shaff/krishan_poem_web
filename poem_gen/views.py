8de3afe3c86bcc8cbfcfb2568b8e81939531021f
Dean Shaff <dshaff001@gmail.com>
2015-01-21 21:24:19 +0400

    Hopefully fixed media file funny business

--- poem_gen/views.py
+++ poem_gen/views.py
@@ -11,6 +11,7 @@ import time
 import os
 
 nltk.data.path.append('./nltk_data/')
+text_dir = os.path.join(settings.MEDIA_ROOT,'texts')
 
 def poem_creator(object1, object2):
 	# print(object1.name, object2.name)
@@ -21,26 +22,34 @@ def poem_creator(object1, object2):
 	# print(text1[0:10], text2[0:10])
 
 	final_poem = str()
-	first = str()
-	second = str()
-	object1.text.open()
-	line_list1 = object1.text.readlines()
-	for line in line_list1:
-		first += line
-	first = first.strip().replace('\r',' ').replace('\n', ' ')
-	first = first.split(' ')
-	first = filter(None, first)
-	# first = re.split('\W+', first)
-	# first = [x.split(' ') for x in first.split('\n')]
-	object2.text.open()
-	line_list2 = object2.text.readlines()
-	for line in line_list2:
-		second += line
-	second = second.strip().replace('\r',' ').replace('\n', ' ')
-	second = second.split(' ')
-	second = filter(None, second)
-	# second = re.split('\W+', second)
-	# second = [x.split(' ') for x in second.split('\n')]
+	if isinstance(object1, TextInput) and isinstance(object2, TextInput):	
+		first = str()
+		second = str()
+		object1.text.open()
+		line_list1 = object1.text.readlines()
+		for line in line_list1:
+			first += line
+		first = first.strip().replace('\r',' ').replace('\n', ' ')
+		first = first.split(' ')
+		first = filter(None, first)
+		# first = re.split('\W+', first)
+		# first = [x.split(' ') for x in first.split('\n')]
+		object2.text.open()
+		line_list2 = object2.text.readlines()
+		for line in line_list2:
+			second += line
+		second = second.strip().replace('\r',' ').replace('\n', ' ')
+		second = second.split(' ')
+		second = filter(None, second)
+
+	elif isinstance(object1, str) and isinstance(object2, str):
+		first = open(os.path.join(text_dir,object1)).read().strip().replace('\r',' ').replace('\n', ' ')
+		first = first.split(' ')
+		first = filter(None, first)
+
+		second = open(os.path.join(text_dir,object2)).read().strip().replace('\r',' ').replace('\n', ' ')
+		second = second.split(' ')
+		second = filter(None, second)
 
 	conj = ['CC', 'IN', 'DT']
 	verbs = ['VBZ', 'VBG', 'VBD', 'VBN', 'VBP']
@@ -124,15 +133,36 @@ def index(request):
 
 	context = RequestContext(request)
 	files_in_text = os.listdir("{}/{}".format(settings.MEDIA_ROOT,"texts"))
+	# print(files_in_text)
 	list_of_objects = TextInput.objects.all()
+	print(isinstance(list_of_objects[0], TextInput))
+	# print(repr(list_of_objects[0].text.path))
+	# print("DickensTaleofTwo.txt".strip(".txt"))
+	# print("DickensTaleofTwo.txt".strip(".txt") in str(list_of_objects[0].text))
+	available_files = []
+	for file_name in files_in_text:
+		for object in list_of_objects:
+			if file_name.strip(".txt") in str(object.text.path):
+				available_files.append(file_name)
+	print(available_files)
 	try:
-		poem = poem_creator(list_of_objects[0], list_of_objects[1])
+		poem = poem_creator(available_files[0], available_files[1])
 	except (IndexError, IOError) as e:
 		list_of_objects = []
 		poem = "No poem!"
 		logging.exception(e)
 		files_in_text = os.listdir("{}/{}".format(settings.MEDIA_ROOT,"texts"))
 		logging.info(files_in_text)
+
+	if request.method == "GET":
+		request_dict = dict(request.GET)
+		books = []
+		print(request_dict)
+		for name in request_dict.keys():
+			if name in list_of_objects:
+				pass
+	else:
+		pass
 
 	return render_to_response('poem_gen/index.html', {'books':list_of_objects, 'poem':poem, 'files':files_in_text}, context_instance=context)
 
