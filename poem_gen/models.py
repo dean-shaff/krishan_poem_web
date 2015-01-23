2a8967a0811fe920f17736a8ec9511580c9e56ba
Dean Shaff <dshaff001@gmail.com>
2015-01-23 12:13:30 +0400

    Continuing to mess with media stuff

--- poem_gen/models.py
+++ poem_gen/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 import os
+from poem_maker import settings
 
-def get_file_path(instance, filename):
-	return os.path.join('texts', filename)
+# def get_file_path(instance, filename):
+# 	return os.path.join('texts', filename)
 
 class TextInput(models.Model):
 	name = models.CharField(max_length = 200, blank=False)
 	author = models.CharField(max_length = 200, blank=True)
-	text = models.FileField(upload_to=get_file_path,blank=False)
+	text = models.FileField(upload_to=os.path.join(settings.MEDIA_ROOT, 'texts'),blank=False)
 
 	def __unicode__(self):
 		
